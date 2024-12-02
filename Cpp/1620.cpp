c8129f9c1f39e089149f5ed53e2422482335cf10
lenamin <lenaminn@gmail.com>
2024-12-02 18:24:12 +0900

    1620 정답코드

--- Cpp/1620.cpp
+++ Cpp/1620.cpp
@@ -1,5 +1,5 @@
+/*
 #include <bits/stdc++.h>
-#include <cctype>
 using namespace std;
 
 int n, k;
@@ -35,4 +35,42 @@ int main() {
     } 
   }
   return 0;
+}*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, k;
+
+
+int main() {
+
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+  
+  cin >> n >> k;
+  map<string, int> mp;
+  map<int, string> mp2;
+  for (int i = 1; i <= n; i++) {
+    int idx = i;
+    string pkc;
+
+    cin >> pkc;
+    mp.insert({pkc, i});
+    mp2.insert({i, pkc});
+  }  
+
+  for (int i = 0; i < k; i++) {
+    string s;
+    cin >> s;
+
+    // 문자열이면 
+    if (atoi(s.c_str()) == 0) {
+      auto q = mp[s];
+      cout << q << '\n';
+    } else {
+      int q = stoi(s);
+      cout << mp2[q] << '\n';
+    }
+  }
 }
