fa72b029ba65dccf264fe4a646d8ff341bc910d3
lenamin <lenaminn@gmail.com>
2025-03-13 14:41:31 +0900

    [프로그래머스] 조건에 부합하는 중고거래 상태 조회하기 (Lv2)
    CASE WHEN, ELT + FIELD

---
+++ SQL/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,13 @@
+-- 2022년 10월 5일 등록된 중고거래 게시물의 
+-- 게시글 ID, 작성자 ID, 게시글 제목, 가격, 거래상태 조회 
+-- SALE : 판매중, RESERVED : 예약중, DONE : 거래완료
+# SELECT * FROM USED_GOODS_BOARD;
+
+SELECT BOARD_ID, WRITER_ID, TITLE, PRICE, (
+        CASE 
+            WHEN STATUS = "RESERVED" THEN "예약중"
+            WHEN STATUS = "DONE" THEN "거래완료"
+            WHEN STATUS = "SALE" THEN "판매중" END) AS STATUS 
+FROM USED_GOODS_BOARD
+WHERE YEAR(CREATED_DATE) = 2022 AND MONTH(CREATED_DATE) = 10 AND DAY(CREATED_DATE) = 5
+ORDER BY BOARD_ID DESC
