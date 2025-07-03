948a9d103a48a63a67a5de8fec990135fa9975c1
lenamin <lenaminn@gmail.com>
2025-07-03 11:55:53 +0900

    [프로그래머스] 삼총사
    Brute Force, 3중 for문

---
+++ Cpp/BruteForce/삼총사.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(vector<int> number) {
+    int answer = 0;
+    vector<int> twin(3); // 삼총사 자리 
+    
+    for (int i = 0; i < number.size(); i++) {
+        for (int j = i + 1; j < number.size(); j++) {
+            for (int k = j + 1; k < number.size(); k++) {
+                if (number[i] + number[j] + number[k] == 0) {
+                    answer++;
+                }
+            }
+        }
+    }    
+    return answer;
+}
