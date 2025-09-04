b5e53c5f3ebdcc97fe94739fb0454f4a1452a751
lenamin <lenaminn@gmail.com>
2025-09-04 14:24:17 +0900

    [1966] 프린터 큐

---
+++ Cpp/Stack&Queue/1966.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int k; // 총 테케 개수 
+int n; // 문서의 개수 
+int m; // 몇번째에 놓여있는지 궁금한 인덱스 
+int cnt; 
+int targetidx;
+
+
+// map<int, int> mp;
+
+int main() {
+    // 가장 첫번재 우선순위 조사 
+    // m번째 자리에 있는 수 target 으로 일단 꺼내놓고 
+    // 첫번째 수의 우선순위보다 높은게 나머지에 하나라도 있으면 맨 뒤로 보내기 
+    cin >> k;
+
+    for (int i = 0; i < k; i++) {
+        cnt = 0;
+        cin >> n >> m;
+
+        queue<pair<int, int>> q; // 우선순위가 있는 순서 
+        priority_queue<int> pq;
+
+        for (int j = 0; j < n; j++) {
+            
+            int tmp;
+            cin >> tmp;
+
+            q.push({j, tmp});
+            pq.push(tmp);
+        }
+        
+
+        while (!q.empty()) {
+            int curMax = pq.top();
+            auto now = q.front();
+            
+            q.pop();
+            
+            if (now.second == curMax) { // 출력
+                
+                pq.pop();
+                cnt++;
+
+                if (m == now.first) {
+                    break;
+                } 
+            } else {
+                q.push(now);
+            } 
+        }
+        cout << cnt << '\n';
+    }
+}
